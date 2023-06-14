febcb9e04afa8ca85d21ccee30108c8e135823a4
AI kawagishi <ne201062@senshu-u.jp>
2023-06-14 21:16:11 +0900

    完成

--- index.js
+++ index.js
@@ -1,22 +1,69 @@
 const express = require('express');
-
+const app = express();
 const PORT = 8080;
 
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.set("view engine", "ejs");
-app.use("/public", express.static(__dirname + "/public"));
-
-let dataString = "";
-
-app.get('/', (req, res) => {
-  res.render('top.ejs', { data: dataString });
-});
-
-app.post('/', (req, res) => {
-  dataString = req.body.data;
-  res.render('top.ejs', { data: dataString });
-});
-
 app.listen(process.env.PORT || PORT);
+
+class Product {
+    constructor(id, name, price) {
+      this.id = id;
+      this.name = name;
+      this.price = price;
+    }
+  }
+
+  class ProductDB {
+    constructor() {
+      this.productNum = 0;
+      this.products = [];
+    }
+  
+    add(name, price) {
+      this.products.push(new Product(this.productNum, name, price));
+      this.productNum++;
+    }
+  }
+  
+let foods = new ProductDB();
+
+foods.add("ポッキー", 280);
+foods.add("きのこの山", 190);
+foods.add("たけのこの里", 190);
+foods.add("たけのこの丘", 190);
+//初期データ挿入
+  //何もなし（全データ表示）
+  app.get('/', (req, res) => {
+    res.contentType('json');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.send({ result: foods.length, data: foods });
+  });
+  
+  app.get('/request', (req, res) => {
+    let result = { err: "", result: 0, data: [] };
+    res.contentType('json');
+    res.header('Access-Control-Allow-Origin', '*');
+    foods.products.forEach(e => {
+    //   if (e.name == req.query.cmd) {
+    //     result.result++;
+    //     result.data.push(e);
+    //   }
+      if(req.query.cmd == "search"){
+        if (e.name.indexOf(req.query.name) !== -1) {
+            result.result++;
+            result.data.push(e);
+          }
+        if (req.query.name == null){
+            result.err ="search name not specified";
+        }
+      }
+    });
+    if(req.query.cmd == "all"){
+        res.send({ result: foods.length, data: foods });
+      }
+    res.send(result);
+  });
+
+  app.get('/add', (req, res) => {
+    foods.add(req.query.name,req.query.price)
+    res.send({ result: foods.length, data: foods });
+  });
