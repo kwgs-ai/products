bd25bdc0e84218c8b3a714fbb08b6635ca82e91a
AI kawagishi <ne201062@senshu-u.jp>
2023-06-15 19:06:12 +0900

    エラーメッセ時追加

--- index.js
+++ index.js
@@ -30,12 +30,11 @@ foods.add("ポッキー", 280);
 foods.add("きのこの山", 190);
 foods.add("たけのこの里", 190);
 // foods.add("たけのこの丘", 190);
-//初期データ挿入
-  //何もなし（全データ表示）
+
   app.get('/', (req, res) => {
     res.contentType('json');
     res.header('Access-Control-Allow-Origin', '*');
-    res.send({ result: foods.length, data: foods });
+    res.send({ err: "", result: foods.products.length, data: foods });
   });
   
   app.get('/request', (req, res) => {
@@ -43,27 +42,38 @@ foods.add("たけのこの里", 190);
     res.contentType('json');
     res.header('Access-Control-Allow-Origin', '*');
     foods.products.forEach(e => {
-    //   if (e.name == req.query.cmd) {
-    //     result.result++;
-    //     result.data.push(e);
-    //   }
       if(req.query.cmd == "search"){
         if (e.name.indexOf(req.query.name) !== -1) {
             result.result++;
             result.data.push(e);
           }
         if (req.query.name == null){
+            //searchnameの指定がないときのエラー
             result.err ="search name not specified";
         }
       }
     });
     if(req.query.cmd == "all"){
-        res.send({ result: foods.length, data: foods });
+      result.result = foods.products.length
+      result.data = foods
       }
+    if(req.query.cmd != "all" && req.query.cmd != "search"){
+      //allかsearch以外のコマンドが指定されt場合はエラーを返す
+      result.err = "this command can not use";
+      result = { err: "", result: foods.products.length, data: foods }
+    }
     res.send(result);
   });
 
   app.get('/add', (req, res) => {
-    foods.add(req.query.name,req.query.price)
-    res.send({ result: foods.length, data: foods });
+    let result = { err: "", result: 0, data: [] }
+    if(req.query.name == null || req.query.price == null){
+      //nameまたはpriceの指定がない場合はエラーを返す
+      result.err = "add name or price not specified";
+    }else{
+      foods.add(req.query.name,req.query.price)
+      //追加後全表示
+      result = { err: "", result: foods.products.length, data: foods }
+    }
+    res.send(result);
   });
